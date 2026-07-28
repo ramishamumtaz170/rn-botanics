f5d15fd33dfa4a1705aa519bfda05cabe5afbda6
Ramisha Mumtaz <ramishamumtaz170@gmail.com>
2026-07-29 02:40:15 +0500

    spacingg

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,7 +4,7 @@ import OrderSummary from "@/app/components/checkout/OrderSummary";
 export default function CheckoutPage() {
   return (
     <main className="bg-[#F8F5EF] min-h-screen py-5">
- <div className="max-w-7xl mx-auto px-7 sm:px-8 lg:px-10">
+ <div className="max-w-7xl mx-auto px-7 sm:px-8 lg:px-8">
         <div className="max-w-3xl mx-auto text-center mb-3">
   <h1 className="text-3xl font-bold text-[#2E473B]">
     Checkout
@@ -14,7 +14,7 @@ export default function CheckoutPage() {
     You're just one step away from experiencing botanical hair care.
   </p>
 </div>
-        <div className="grid lg:grid-cols-[1fr_1fr] gap-4">
+        <div className="grid lg:grid-cols-[1fr_1fr] gap-2">
 
           <CheckoutForm />
 
